70bee20059c3827d28d5d9b0307d1fa465f841f7
Nizam-04 <nizamodenk@gmail.com>
2022-07-13 14:34:27 +0700

    Menambahkan bagian cookie (remember me) di file login.php dan logout.php

--- login.php
+++ login.php
@@ -1,10 +1,25 @@
 <?php
   session_start();
+  require 'functions.php';
+  // Cek cookie
+  if(isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
+    $id = $_COOKIE['id'];
+    $key = $_COOKIE['key'];
+
+    // Ambil username berdasarkan id
+    $result = mysqli_query($conn, "SELECT username FROM user WHERE id = $id");
+    $row = mysqli_fetch_assoc($result);
+
+    // Cek cookie dan username
+    if($key === hash('sha256', $row["username"])) {
+      $_SESSION["login"] = true;
+    }
+  }
+
   if(isset($_SESSION["login"])) {
     header("Location: index.php");
     exit;
   }
-  require 'functions.php';
 
   if(isset($_POST["login"])) {
     $username = $_POST["username"];
@@ -19,6 +34,14 @@
       if(password_verify($password, $row["password"])) {
         // Set session
         $_SESSION["login"] = true;
+
+        // Cek remember me
+        if(isset($_POST["remember"])) {
+          // Buat cookie
+
+          setcookie('id', $row["id"], time() + 60);
+          setcookie('key', hash('sha256', $row["username"]), time() + 60);
+        }
 
         header("Location: index.php");
         exit;
@@ -52,6 +75,10 @@
       <li>
         <label for="password">Password :</label>
         <input type="password" name="password" id="password">
+      </li>
+      <li>
+        <input type="checkbox" name="remember" id="remember">
+        <label for="remember">Remember Me</label>
       </li>
       <li>
         <button type="submit" name="login">Login</button>
--- logout.php
+++ logout.php
@@ -3,6 +3,10 @@
   $_SESSION = [];
   session_unset();
   session_destroy();
+
+  setcookie('id', '', time() - 3600);
+  setcookie('key', '', time() - 3600);
+  
   header("Location: login.php");
   exit;
 ?>
